cdeb3e49f8bac6be5abd2afd5ec32d2ccbc90bba
yoann foreau <yoann.foreau@ito.am>
2021-02-05 01:45:08 +0100

    Move des functions together

--- src/cyphers/des.c
+++ src/cyphers/des.c
@@ -6,12 +6,46 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 03:17:04 by yforeau           #+#    #+#             */
-/*   Updated: 2021/02/04 01:07:37 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/02/05 01:42:51 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "des.h"
 #include "des_const.h"
+
+# define BIT(x, n) (((x) >> (n)) & 0x01)
+
+uint64_t		des_sbox(uint64_t x)
+{
+	uint64_t	y;
+	size_t		j;
+	size_t		k;
+
+	y = 0;
+	for (size_t i = 0; i < SBOX_NB; ++i)
+	{
+		y = y << 4;
+		j = (BIT(x, 47) << 1) | BIT(x, 42);
+		k = (BIT(x, 46) << 3) | (BIT(x, 45) << 2)
+			| (BIT(x, 44) << 1) | BIT(x, 43);
+		y |= g_sbox[i][j][k] & 0x0f;
+		x = x << 6;
+	}
+	return (y);
+}
+
+uint64_t		des_permute(uint64_t x, const t_des_const *p)
+{
+	uint64_t	y;
+
+	y = 0;
+	for (size_t i = 0; i < p->output_size; ++i)
+	{
+		y = y << 1;
+		y |= BIT(x, p->input_size - p->bit[i]);
+	}
+	return (y);
+}
 
 static uint64_t	des_f(uint64_t x, uint64_t key)
 {
--- src/cyphers/des_const.c
+++ src/cyphers/des_const.c
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 19:33:10 by yforeau           #+#    #+#             */
-/*   Updated: 2021/02/04 00:29:05 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/02/05 01:43:06 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -160,37 +160,3 @@ const uint64_t		g_sbox[SBOX_NB][SBOX_ROW][SBOX_COL] = {
 		{ 2,	1,	14,	7,	4,	10,	8,	13,	15,	12,	9,	0,	3,	5,	6,	11 }
 	}
 };
-
-# define BIT(x, n) (((x) >> (n)) & 0x01)
-
-uint64_t	des_permute(uint64_t x, const t_des_const *p)
-{
-	uint64_t	y;
-
-	y = 0;
-	for (size_t i = 0; i < p->output_size; ++i)
-	{
-		y = y << 1;
-		y |= BIT(x, p->input_size - p->bit[i]);
-	}
-	return (y);
-}
-
-uint64_t	des_sbox(uint64_t x)
-{
-	uint64_t	y;
-	size_t		j;
-	size_t		k;
-
-	y = 0;
-	for (size_t i = 0; i < SBOX_NB; ++i)
-	{
-		y = y << 4;
-		j = (BIT(x, 47) << 1) | BIT(x, 42);
-		k = (BIT(x, 46) << 3) | (BIT(x, 45) << 2)
-			| (BIT(x, 44) << 1) | BIT(x, 43);
-		y |= g_sbox[i][j][k] & 0x0f;
-		x = x << 6;
-	}
-	return (y);
-}
