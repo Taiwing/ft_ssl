256656c586a83bf3639a4423c5aa45e1f5040b5f
yoann foreau <yoann.foreau@ito.am>
2021-04-14 09:09:30 +0200

    Finish rsa print text option

--- src/rsa/cmd_rsa.c
+++ src/rsa/cmd_rsa.c
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 06:54:37 by yforeau           #+#    #+#             */
-/*   Updated: 2021/04/14 08:07:03 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/04/14 09:08:28 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ static int	parse_options(t_rsa_key *key, int *outfd,
 		return (!!ft_dprintf(2,
 			"ft_ssl: %s: Only private keys can be checked\n", cmd->name));
 	if (opt[RSA_INFORM].is_set
-			&& ft_ignore_case_strcmp(opt[RSA_INFORM].value, "DER"))
+			&& ft_ignore_case_strcmp(opt[RSA_INFORM].value, "PEM"))
 		return (!!ft_dprintf(2, "ft_ssl: %s: Invalid format \"%s\" "
 			"for -inform\n", cmd->name, opt[RSA_INFORM].value));
 	if (opt[RSA_OUTFORM].is_set
-			&& ft_ignore_case_strcmp(opt[RSA_OUTFORM].value, "DER"))
+			&& ft_ignore_case_strcmp(opt[RSA_OUTFORM].value, "PEM"))
 		return (!!ft_dprintf(2, "ft_ssl: %s: Invalid format \"%s\" "
 			"for -outform\n", cmd->name, opt[RSA_OUTFORM].value));
 	if (parse_rsa_key(key, cmd->name, &gk))
@@ -39,18 +39,70 @@ static int	parse_options(t_rsa_key *key, int *outfd,
 	return (*outfd < 0);
 }
 
-int		cmd_rsa(const t_command *cmd, t_cmdopt *opt, char **args)
+static int	print_text_rsa_key(int outfd, t_rsa_key *key)
+{
+	if (key->is_pub)
+		ft_dprintf(outfd, "RSA Public-Key: (64 bit)\n"
+			"Modulus: %1$llu (%1$#llx)\n"
+			"Exponent: %2$llu (%2$#llx)\n",
+			key->n, key->e);
+	else
+		ft_dprintf(outfd, "RSA Private-Key: (64 bit, 2 primes)\n"
+			"modulus: %1$llu (%1$#llx)\n"
+			"publicExponent: %2$llu (%2$#llx)\n"
+			"privateExponent: %3$llu (%3$#llx)\n"
+			"prime1: %4$llu (%4$#llx)\n"
+			"prime2: %5$llu (%5$#llx)\n"
+			"exponent1: %6$llu (%6$#llx)\n"
+			"exponent2: %7$llu (%7$#llx)\n"
+			"coefficient: %8$llu (%8$#llx)\n",
+			key->n, key->e, key->d, key->p, key->q,
+			key->exp1, key->exp2, key->coeff);
+	return (0);
+}
+
+static int	rsa_check_key(int outfd, t_rsa_key *key)
+{
+	(void)key;
+	//TODO: actually check key
+	ft_dprintf(outfd, "RSA key ok\n");
+	return (0);
+}
+
+static int	create_output_key(int outfd, t_rsa_key *key_in,
+	t_cmdopt *opt, const t_command *cmd)
+{
+	t_rsa_key	key_out;
+	int			ret;
+
+	(void)outfd;
+	(void)opt;
+	(void)cmd;
+	ret = 0;
+	ft_memcpy((void *)&key_out, (void *)key_in, sizeof(t_rsa_key));
+	ft_bzero((void *)&key_in->des, sizeof(t_des_ctx));
+	//TODO: modify and print new key
+	return (ret);
+}
+
+int			cmd_rsa(const t_command *cmd, t_cmdopt *opt, char **args)
 {
 	int			ret;
 	int			outfd;
-	t_rsa_key	key_in;
-	t_rsa_key	key_out;
+	t_rsa_key	key;
 
 	(void)args;
-	(void)key_out;
 	outfd = 1;
-	ft_bzero((void *)&key_in, sizeof(t_rsa_key));
-	ret = parse_options(&key_in, &outfd, opt, cmd);
+	ft_bzero((void *)&key, sizeof(t_rsa_key));
+	ret = parse_options(&key, &outfd, opt, cmd);
+	if (!ret && opt[RSA_TEXT].is_set)
+		print_text_rsa_key(outfd, &key);
+	if (!ret && opt[RSA_MODULUS].is_set)
+		ft_dprintf(outfd, "Modulus=%llX\n", key.n);
+	if (!ret && opt[RSA_CHECK].is_set)
+		ret = rsa_check_key(outfd, &key);
+	if (!ret && !opt[RSA_NOOUT].is_set)
+		ret = create_output_key(outfd, &key, opt, cmd);
 	if (outfd > 1)
 		close(outfd);
 	return (ret);
