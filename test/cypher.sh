dd2f3577b1b1bfa5c7e1e9069ef2abfe00614359
Yoann FOREAU <yforeau@e3r12p9.clusters.42paris.fr>
2022-04-14 11:21:22 +0200

    test: des block modes implemented by openssl

--- test/cypher.sh
+++ test/cypher.sh
@@ -42,3 +42,25 @@ str="Zm9vYmFyCg=="
 mine=$(echo "$str" | ./ft_ssl base64 -d)
 orig=$(echo "$str" | openssl base64 -d)
 test_result 5 "$orig" "$mine"
+
+orig="one deep secret ecb"
+key="6162636461626364"
+iv="0011223344556677"
+mine=$(echo $orig | ./ft_ssl des-ecb -a -k $key -v $iv \
+	| openssl des-ecb -d -a -K $key -iv $iv)
+test_result 6 "$orig" "$mine"
+
+orig="one deep secret cbc"
+mine=$(echo $orig | ./ft_ssl des-cbc -a -k $key -v $iv \
+	| openssl des-cbc -d -a -K $key -iv $iv)
+test_result 7 "$orig" "$mine"
+
+orig="one deep secret cfb"
+mine=$(echo $orig | ./ft_ssl des-cfb -a -k $key -v $iv \
+	| openssl des-cfb -d -a -K $key -iv $iv)
+test_result 8 "$orig" "$mine"
+
+orig="one deep secret ofb"
+mine=$(echo $orig | ./ft_ssl des-ofb -a -k $key -v $iv \
+	| openssl des-ofb -d -a -K $key -iv $iv)
+test_result 9 "$orig" "$mine"
